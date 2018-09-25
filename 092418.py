4d2d575db87e57273c79f59aaec81373543872a9
Beth Damato <bethadamato@gmail.com>
2018-09-24 22:49:37 -0400

    updating exercises file

--- 092418.py
+++ 092418.py
@@ -1,5 +1,26 @@
-bottles = 99
+# bottles = 99
 
-def song(bottles):
-    while bottles > 0:
+# def song(bottles):
+#     while bottles > 0:
         
+
+
+
+##FOR LOOPS
+
+# my_bag = [
+#     'rocks',
+#     'gravy',
+#     'a tiny kitten',
+#     'gems'
+# ]
+
+# for item in my_bag:
+#     print(item)
+
+##another
+for x in range(5, 0, -1):
+    print(x * 3)
+
+
+##FUNCTIONS
