cbbd0281747878767413e9704d096c31b50c54da
Beth Damato <bethadamato@gmail.com>
2018-09-30 23:17:41 -0400

    updated player and items logic

--- glamping/chi.py
+++ glamping/chi.py
@@ -10,22 +10,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         # self.inventory = {}
 
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+    # def update(self, movex, movey, items):
+    #     self.moveSprite(movex, movey)
+    #     self.itemsCollision(items)
+    #     self.render()
 
-        #Keep player on the screen
-        if self.rect.left < 0:
-            self.rect.left = 0
-        elif self.rect.right > 512:
-            self.rect.right = 512
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        elif self.rect.bottom >= 480:
-            self.rect.bottom = 480
+    def itemsCollision(self, items):
+        pygame.sprite.spritecollide(self, items, True)
--- glamping/game.py
+++ glamping/game.py
@@ -3,19 +3,19 @@ from chi import Player
 from pygame.locals import *
 from supplies import Item
 
-
-
-
 width = 512
 height = 480
 
-# Define some colors
+
+
+# COLOR DEFINITIONS ------------------
 black = (0, 0, 0)
 white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
 blue = (0, 0, 255)
 
+# INITIALIZE GAME SQUARE & BACKGROUND MUSIC ----------------
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load('sounds/Friends.ogg')
@@ -27,7 +27,7 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('NO TIME TO GLAMP!')
 
 
-##opening images
+# OPENING IMAGES --------------------------------
 tentImg = pygame.image.load('images/tent.gif').convert()
 background = pygame.image.load('images/background.png').convert()
 logsImg = 'images/logs.png'
@@ -35,70 +35,78 @@ fireImg = 'images/fire.png'
 fishImg = 'images/fish.png'
 waterImg = 'images/water.png'
 
-##MAIN PLAYER (chihuahua)
+# MAIN PLAYER (chihuahua) & ITEM SPRITES ----------------
+all_sprites = pygame.sprite.Group()
+
 player = Player(0, 0, 'images/chi.gif')
 player.rect.x = 165
 player.rect.y = 190
-player_list = pygame.sprite.Group()
-player_list.add(player)
 player_speed = 15
-
-##ITEMS TO COLLECT
-items = pygame.sprite.Group()
 logs = Item(350, 50, {'logs ': 1}, logsImg)
 fire = Item(350, 125, {'fire ': 1}, fireImg)
 fish = Item(340, 240, {'fish ': 1}, fishImg)
 water = Item(350, 325, {'water ': 1}, waterImg)
-items.add(logs, fire, fish, water)
 
+all_sprites.add(player, logs, fire, fish, water)
 
-
-##collected items score
+# SCORING & COLLECTION ---------------
 def items_collected (screen, x, y, count):
     font = pygame.font.Font(None, 36)
     text = font.render("Items Collected: "+ str(count), True, white)
     screen.blit(text,(100, 350))
 score = 0
 
-##timer coundown
+## TIMER ---------------------------
 # def count_down (screen, x, y, count):
 #     font = pygame.font.Font (None, 36)
 #     text = 10, '10'.rjust(3)
 #     pygame.time.set_timer(pygame.USEREVENT, 1000)
 
 
-
-##game initialization
+# MAIN GAME LOOP _________________
 running = True
-##hides the mouse cursor
-pygame.mouse.set_visible(0)
-
-##main game loop
 while running:
     for event in pygame.event.get():
-        #press escape key to quit game
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
-                raise SystemExit
-            elif event.type == QUIT:
+        #check for closing window
+        if event.type == QUIT:
+            running = False
+            raise SystemExit
+        #check for escape key click
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
                 running = False
                 raise SystemExit
 
-    #key has been pressed
+    #move player around if arrow key pressed
     pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[K_UP]:
+        player.rect.move_ip(0, -5)
+    if pressed_keys[K_DOWN]:
+        player.rect.move_ip(0, 5)
+    if pressed_keys[K_LEFT]:
+        player.rect.move_ip(-5, 0)
+    if pressed_keys[K_RIGHT]:
+        player.rect.move_ip(5, 0)
 
-    # player.update(pressed_keys)
-    player.update(pressed_keys)
+    #Keep player on the screen
+    if player.rect.left < 0:
+        player.rect.left = 0
+    elif player.rect.right > 512:
+        player.rect.right = 512
+    if player.rect.top <= 0:
+        player.rect.top = 0
+    elif player.rect.bottom >= 480:
+        player.rect.bottom = 480
 
+    # UPDATE ------------------
+    all_sprites.update()
+    
 
-    ##game display
+    # DRAWING / RENDERING ON SCREEN -----------------------
     pygame.display.update()
     screen.fill((0,0,0))
     screen.blit(background, (0,0))
     screen.blit(tentImg, (width * 0.03, height * 0.35))
-    player_list.draw(screen)
-    items.draw(screen)
+    all_sprites.draw(screen)
     items_collected(screen, 20, 20, score) 
     pygame.display.flip()
     clock.tick(60)
